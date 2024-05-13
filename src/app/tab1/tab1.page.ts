8c029eb7eacfcc80d7f24fda2c9300c8c973a621
Aditya Raj Aryan <adityaraja338@gmail.com>
2024-05-13 15:37:49 +0530

    remote setup

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,101 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Router } from '@angular/router';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements AfterViewInit {
+  @ViewChild('overallCanvas')
+  private overallCanvas!: ElementRef;
 
-  constructor() {}
+  @ViewChild('previousCanvas')
+  private previousCanvas!: ElementRef;
 
+  overallChart: any;
+  previousChart: any;
+
+  constructor(
+    private router:Router
+  ) {}
+
+  ngAfterViewInit() {
+    this.overallChart = new Chart(this.overallCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['BJP', 'Congress', 'AAP', 'CPM', 'SP'],
+        datasets: [
+          {
+            data: [50, 29, 15],
+            backgroundColor: [
+              '#8555FD',
+              '#C1B2FF',
+              '#E4E0FA',
+            ],
+            // hoverBackgroundColor: [
+            //   '#FFCE56',
+            //   '#FF6384',
+            //   '#36A2EB',
+            // ],
+            borderJoinStyle: 'round',
+          },
+        ],
+      },
+      options: {
+        cutout: '75%',
+        radius: 58,
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      },
+    });
+    this.previousChart = new Chart(this.previousCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['BJP', 'Congress', 'AAP', 'CPM', 'SP'],
+        datasets: [
+          {
+            data: [69, 29, 15],
+            backgroundColor: [
+              '#8555FD',
+              '#C1B2FF',
+              '#E4E0FA',
+            ],
+            borderJoinStyle: 'round',
+          },
+        ],
+      },
+      options: {
+        cutout: '75%',
+        radius: 58,
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      },
+    });
+  }
+
+  navigateToTab2() {
+    // Navigate to the desired tab programmatically
+    setTimeout(()=> {this.router.navigateByUrl('/tabs/performances');}, 220);
+  }
+
+  handleRefresh(event:any) {
+    setTimeout(() => {
+      // Any calls to load data go here
+      event.target.complete();
+    }, 2000);
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,63 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  performances:any = [
+    {
+      id:1,
+      subject: 'Test 1',
+      startTime: '2024-05-18T18:30:00.000Z',
+      endTime: '2024-05-18T19:30:00.000Z',
+      score: 68,
+      total: 90,
+      accuracy: Math.round(68/90 *10000)*100,
+      correct: 14,
+      incorrect: 2,
+      unattempted: 2,
+    },
+    {
+      id:2,
+      subject: 'Test 2',
+      startTime: '2024-05-19T18:30:00.000Z',
+      endTime: '2024-05-19T19:30:00.000Z',
+      score: 70,
+      total: 90,
+      accuracy: Math.round(70/90 *10000)*100,
+      correct: 14,
+      incorrect: 0,
+      unattempted: 4,
+    },
+    {
+      id:3,
+      subject: 'Test 3',
+      startTime: '2024-05-21T18:30:00.000Z',
+      endTime: '2024-05-21T19:30:00.000Z',
+      score: 78,
+      total: 90,
+      accuracy: Math.round(78/90 *10000)*100,
+      correct: 16,
+      incorrect: 2,
+      unattempted: 0,
+    },
+    {
+      id:4,
+      subject: 'Test 4',
+      startTime: '2024-05-25T18:30:00.000Z',
+      endTime: '2024-05-25T19:30:00.000Z',
+      score: 57,
+      total: 90,
+      accuracy: Math.round(57/90 *10000)*100,
+      correct: 12,
+      incorrect: 3,
+      unattempted: 3,
+    },
+  ];
+
   constructor() {}
 
+  handleRefresh(event:any) {
+    setTimeout(() => {
+      // Any calls to load data go here
+      event.target.complete();
+    }, 2000);
+  }
 }
