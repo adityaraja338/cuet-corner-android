589fb9216f7eea855fa392466658953ee82f3553
Aditya Raj Aryan <adityaraja338@gmail.com>
2024-05-19 10:41:00 +0530

    spec changes done

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -8,7 +8,7 @@ import { AddToastService } from './add-toast.service';
   providedIn: 'root',
 })
 export class HttpService {
-  url = 'http://localhost:3000/student/';
+  url = 'https://localhost:3000/student/';
 
   constructor(
     private http: HttpClient,
